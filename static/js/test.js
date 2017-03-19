557dc6971233d99cc875325aa89d8894a17992ac
Ian Agpawa <agpawaji@gmail.com>
2017-03-19 18:54:06 -0400

    refactor: Change iframe

--- static/js/test.js
+++ static/js/test.js
@@ -1,11 +1,19 @@
 function startPage(){
+    // var thing = document.getElementsByClassName("test");
+    // console.log(thing);
 
-    $.getJSON($SCRIPT_ROOT + '/playlists/JSON', function(data) {
-        playlists = data["Playlists"];
-        for (playlist in playlists){
-            console.log(playlists[playlist])
-        }
-      });
+    // $(".ytp-large-play-button").click(function() {
+    //     // var thing = $(".ytp-title-link").attr('href')
+    //     console.log("here")
+    // });
+
+    // $("#test").html('WORKINTG')
+    // $.getJSON($SCRIPT_ROOT + '/playlists/JSON', function(data) {
+    //     playlists = data["Playlists"];
+    //     for (playlist in playlists){
+    //         console.log(playlists[playlist])
+    //     }
+    //   });
 };
 
 $(document).ready(startPage);
