56d76e373b81be84e99e942a402056598bf38b3f
Ian Agpawa <agpawaji@gmail.com>
2017-05-27 15:41:04 -0400

    refactor: Update facebook login

--- main.py
+++ main.py
@@ -718,48 +718,51 @@ def fbconnect():
     Returns:
         Renders login page, then redirects to home page.
     '''
+
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
     access_token = request.data
-    print "access token received %s " % access_token
 
     app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
         'web']['app_id']
     app_secret = json.loads(
         open('fb_client_secrets.json', 'r').read())['web']['app_secret']
-    url = ("https://graph.facebook.com/oauth/access_token?grant_type="
-           "fb_exchange_token&client_id=%s&client_secret=%s&"
-           "fb_exchange_token=%s") % (app_id, app_secret, access_token)
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+        app_id, app_secret, access_token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
+
 
     # Use token to get user info from API
-    userinfo_url = "https://graph.facebook.com/v2.4/me"
+    userinfo_url = "https://graph.facebook.com/v2.8/me"
     # strip expire tag from access token
-    token = result.split("&")[0]
+    token = result.split(',')[0].split(':')[1].replace('"', '')
 
-    url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
+
+
+    url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
-    # print "url sent for API access:%s"% url
-    # print "API JSON result: %s" % result
     data = json.loads(result)
+    # sys.stdout = open('output.logs', 'w')
+    # print (data["name"]) # Nothing appears below
+    # print (data["email"]) # Nothing appears below
+    # print (data["id"]) # Nothing appears below
+    # sys.stdout = sys.__stdout__ # Reset to the standard output
+    # open('output.logs', 'r').read()
+    # return "OK"
     login_session['provider'] = 'facebook'
     login_session['username'] = data["name"]
     login_session['email'] = data["email"]
     login_session['facebook_id'] = data["id"]
 
-    # The token must be stored in the login_session in order to properly
-    # logout, let's strip out the information before the equals sign in
-    # our token
-    stored_token = token.split("=")[1]
-    login_session['access_token'] = stored_token
+    # # The token must be stored in the login_session in order to properly logout, let's strip out the information before the equals sign in our token
+    login_session['access_token'] = token
 
     # Get user picture
-    url = ("https://graph.facebook.com/v2.4/me/picture?%s&redirect=0"
-           "&height=200&width=200") % token
+    url = 'https://graph.facebook.com/v2.8/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
